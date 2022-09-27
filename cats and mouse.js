ebc83011495d67d0333cf4d608d4f4b767932ca7
abyebin <112667787+abyebin@users.noreply.github.com>
2022-09-27 11:47:13 +0530

    Create cats and mouse.js

---
+++ cats and mouse.js
@@ -0,0 +1,31 @@
+function catAndMouse(x, y, z) {
+    
+    let psA = 0;
+    let psB = 0;
+  
+    if (x > z){
+      psA = x - z;
+    }
+    else {
+      psA = z - x;
+         }
+    
+    if (y > z){
+      psB = y - z;
+    }
+    else {
+      psB = z - y;
+         }
+    if (psA > psB){
+      return ("Cat B")
+    }
+    else if (psA < psB){
+      return ("Cat A")
+    }
+    else if (psA === psB){
+      return ("Mouse C")
+    }
+  }
+  
+    let result = catAndMouse(1, 2, 3);
+    console.log(result)
